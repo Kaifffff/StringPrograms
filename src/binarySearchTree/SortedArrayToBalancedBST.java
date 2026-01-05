6088e27a697483059284413eb236bbf3c7acdc4d
kaif <shaikhkaif3258@gmail.com>
2026-01-05 14:07:30 +0530

    Sorted Array To Balanced BST

---
+++ src/binarySearchTree/SortedArrayToBalancedBST.java
@@ -0,0 +1,44 @@
+package binarySearchTree;
+
+import binarySearchTree.MirrorABST.Node;
+
+public class SortedArrayToBalancedBST {
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(int data){
+			this.data = data;
+			this.left = this.right = null;
+		}
+	}
+	
+	
+	public static void preOrder(Node root) {
+		if(root == null) {
+			return ;
+		}
+		
+		System.out.print(root.data + " ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+	
+	public static Node balancedBST(int []arr , int st , int end) {
+		if(st > end) {
+			return null;
+		}
+		
+		int mid = (st + end)/2;
+		Node root = new Node(arr[mid]);
+		root.left = balancedBST(arr , st , mid - 1);
+		root.right = balancedBST(arr, mid+1 , end);
+		return root;
+	}
+	
+	public static void main(String []args) {
+		int values[] = {3,5,6,8,10,11,12};
+		Node root = balancedBST(values , 0 , values.length - 1);
+		preOrder(root);
+	}
+}
